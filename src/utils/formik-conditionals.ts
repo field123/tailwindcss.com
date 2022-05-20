8432ddaac4c3455265bb8fb2970abdad96e93b73
Robert Field <robertfield@quadrimular.com>
2022-05-20 14:09:07 +0100

    feat(signup): dark mode styles for generic form

--- src/utils/formik-conditionals.ts
+++ src/utils/formik-conditionals.ts
@@ -19,7 +19,7 @@ export const formFieldCondClasses = <T>(
   touched: FormikTouched<T>
 ) =>
   clsx(
-    'mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md',
+    'mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md dark:bg-slate-900 dark:text-slate-400 dark:border-slate-500',
     errors[fieldName] &&
       touched[fieldName] &&
       'border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500'
